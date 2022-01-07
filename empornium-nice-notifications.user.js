eab8f2967191b17e761416ee1261d75d44dd1005
vandenium <86036198+vandenium@users.noreply.github.com>
2022-01-06 19:59:56 -0800

    Forum notifications now working.

--- empornium-nice-notifications.user.js
+++ empornium-nice-notifications.user.js
@@ -2,7 +2,7 @@
 // @name        Empornium Nice Notifications
 // @description Clickable notification alerts
 // @namespace   Empornium Scripts
-// @version     1.0.1
+// @version     1.1.0
 // @author      vandenium
 // @grant       none
 // @include     /^https://www\.empornium\.(me|sx|is)\/*/
@@ -10,6 +10,8 @@
 // ==/UserScript==
 
 // Changelog:
+// Version 1.1.0
+//  - Add forum subscription notifications
 // Version 1.0.1
 //  - Bugfix: Fix issue of applying to only first alert.
 // Version 1.0.0
@@ -17,6 +19,68 @@
 // Todo:
 
 const alertBarContainers = document.querySelectorAll('div#alerts div.alertbar');
+const  isSubscriptionNotifications = () => {
+  const el = document.querySelector('a.new-subscriptions');
+  const elTextMatch = /\(([0-9]+)\)/.exec(el.textContent);
+  if (elTextMatch) {
+    return elTextMatch[1]
+  }
+  return false;
+};
+
+const insertAfter = (newNode, existingNode) => existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+
+const subscriptionNotificationCount = isSubscriptionNotifications();
+
+if (subscriptionNotificationCount) {
+
+  // create stylesheet
+  const css = `
+    div.subscription-alerts a:hover { 
+      background-color: #eee;
+    }
+
+    div.subscription-alerts {
+      margin: 0 auto;
+      text-align: center;
+      max-width: 700px;
+      width: 50%;
+    }
+
+    div.subscription-alerts a {
+      background-color: #ccc;
+      color: #333;
+      width: 100%;
+      display: inline-block;
+      vertical-align: middle;
+      text-decoration: none;
+      line-height: 25px;
+      border-radius: 25px;
+      margin: 5px;
+      font-weight: bold;
+    }
+  `;
+  var style = document.createElement('style');
+  style.appendChild(document.createTextNode(css));
+  style.appendChild(document.createTextNode(css));
+  document.querySelector('head').appendChild(style);
+
+  const subscriptionAlertContainer = document.createElement('div');
+  subscriptionAlertContainer.classList.add('subscription-alerts');
+
+  const createNotification = (text) => {
+    const el = document.createElement('a');
+    el.href = '/userhistory.php?action=subscriptions';
+    el.innerText = text;
+    return el;
+  }
+
+  
+  const notification = createNotification(`You have ${subscriptionNotificationCount} new forum notification${subscriptionNotificationCount * 1 === 1 ? '' : 's'}`);
+  subscriptionAlertContainer.append(notification);
+
+  insertAfter(subscriptionAlertContainer, document.getElementById('menu'));
+}
 
 // If no alertbar, exit.
 if (alertBarContainers.length > 0) {
@@ -40,7 +104,26 @@ if (alertBarContainers.length > 0) {
   const rgb = getRGB(originalAlertBackgroundColor);
   const hoverAlertBackgroundColor = `rgb(${rgb.red * 1 - 50}, ${rgb.green * 1 - 50}, ${rgb.blue * 1 - 50})`;
 
-  var css = `div#alerts a:hover{ background-color: ${hoverAlertBackgroundColor} !important;}`;
+  var css = `
+    div#alerts a:hover { 
+      background-color: ${hoverAlertBackgroundColor} !important;
+    }
+
+    div.subscription-alerts a:hover { 
+      background-color: #eee;
+    }
+
+    div#alerts a, div.subscription-alerts a {
+      width: 100%;
+      display: inline-block;
+      vertical-align: middle;
+      text-decoration: none;
+      line-height: 25px;
+      border-radius: 25px;
+      margin: 5px;
+      font-weight: bold;
+    }
+    `;
   var style = document.createElement('style');
 
   if (style.styleSheet) {
@@ -58,19 +141,13 @@ if (alertBarContainers.length > 0) {
 
   const alerts = alertsContainer.querySelectorAll('a');
 
-  alerts.forEach(alert => {
-    alert.style.cssText = `
-      width: 100%;
-      background-color: ${originalAlertBackgroundColor};
-      color: ${originalAlertColor};
-      display: inline-block;
-      vertical-align: middle;
-      text-decoration: none;
-      font-size: 14px;
-      line-height: 25px;
-      border-radius: 25px;
-      margin: 5px;
-      font-weight: bold;
-    `;
-  });
+  const styleAlerts = (alerts) => {
+    alerts.forEach(alert => {
+      alert.style.backgroundColor = originalAlertBackgroundColor;
+      alert.style.color = originalAlertColor;
+      ;
+    })
+  }
+
+  styleAlerts(alerts);
 }
